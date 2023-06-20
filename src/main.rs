ab7ea6f4af8d6e071071a1d7c9acb024fc134a83
Ibrahim Hisham <muhammadibrahimhisham@gmail.com>
2023-06-20 07:06:18 +0500

    get active processes via tasklist

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::collections::HashMap;
+
 use clap::{Parser, Subcommand};
+use tasklist;
+use windows::core::*;
+use windows::{Win32::Foundation::*, Win32::UI::WindowsAndMessaging::*};
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version)]
 #[command(propagate_version = true)]
 struct Cli {
     #[command(subcommand)]
@@ -10,21 +15,47 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    Autostart { toggle: Option<String> },
+    Autostart { toggle: String },
+    Start {},
+    Stop {},
+    List {},
 }
 
-fn main() {
+#[cfg(windows)]
+fn main() -> Result<()> {
     let cli = Cli::parse();
+    let mut active_windows: HashMap<String, i32> = HashMap::new();
 
     match &cli.command {
         Commands::Autostart { toggle } => {
-            if let Some(toggle) = toggle {
-                if toggle == "on" {
-                    println!("Atropos will now boot on startup!")
-                } else if toggle == "off" {
-                    println!("Stopped Atropos from booting on startup!")
-                }
+            if toggle == "on" {
+                println!("Atropos will now boot on startup!")
+            } else if toggle == "off" {
+                println!("Stopped Atropos from booting on startup!")
             }
         }
+        Commands::Start {} => {
+            active_windows = get_active_processes();
+            println!("Started monitoring {} windows", active_windows.keys().len());
+        }
+        Commands::List {} => {}
+        Commands::Stop {} => {}
     }
+    Ok(())
+}
+
+fn get_active_processes() -> HashMap<String, i32> {
+    let mut windows: HashMap<String, i32> = HashMap::new();
+    for i in unsafe { tasklist::Tasklist::new() } {
+        let cpn = match i.get_file_info().get("CompanyName") {
+            Some(h) => h.to_string(),
+            None => "".to_string(),
+        };
+        let des = match i.get_file_info().get("FileDescription") {
+            Some(h) => h.to_string(),
+            None => "".to_string(),
+        };
+        windows.insert(des, 0);
+    }
+    windows
 }
