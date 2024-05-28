e21fd5e4570d700992a84e6c71c2a9b51ae2e836
ibrahim hisham <muhammadibrahimhisham@gmail.com>
2024-05-28 17:17:19 +0500

    provide store_path to daemon

--- laches_mon/src/main.rs
+++ laches_mon/src/main.rs
@@ -1,33 +1,41 @@
-use std::{env, thread, time::{Duration, Instant}};
+use laches::LachesStore;
+use std::{
+    env, thread,
+    time::{Duration, Instant},
+};
 
-fn tick() {
-  println!("monitoring...");
+fn tick(store_path: &str, update_interval: &Duration) {
+    println!("watching: {}", store_path);
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-      eprintln!("usage: laches_mon <update_interval>");
-      return;
-    }
-  
-    let value: u64 = match args[1].parse() {
-      Ok(num) => num,
-      Err(_) => {
-        eprintln!("error: invalid argument provided");
+
+    if args.len() != 3 {
+        eprintln!("usage: laches_mon <update_interval> <path/to/store>");
         return;
-      }
+    }
+
+    println!("info: running daemon (laches_mon)...");
+
+    let update_interval: Duration = match args[1].parse() {
+        Ok(num) => Duration::from_millis(num),
+        Err(_) => {
+            eprintln!("error: invalid argument(s) provided");
+            eprintln!("usage: laches_mon <update_interval>");
+            return;
+        }
     };
 
-  let interval = Duration::from_millis(value); 
-  let mut last_tick = Instant::now();
+    let file_path = args[2].as_str(); //todo: no validation of whether the path is actually in a valid form
+    let mut last_tick = Instant::now();
 
-  loop {
-    let elapsed = last_tick.elapsed();
-    if elapsed >= interval {
-      tick();
-      last_tick = Instant::now();
+    loop {
+        let elapsed = last_tick.elapsed();
+        if elapsed >= update_interval {
+            tick(&file_path, &update_interval);
+            last_tick = Instant::now();
+        }
+        thread::sleep(Duration::from_millis(1));
     }
-    thread::sleep(Duration::from_millis(1)); 
-  }
 }
